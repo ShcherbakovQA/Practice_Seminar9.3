056041ffd7ef72bb9320a4079310eba62c3942e9
Alexander <alex17.05.84@yandex.ru>
2022-11-06 17:34:50 +0300

    Exercise 01 done

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63. Задайте вещественнон значение N. Напишите программу, 
+// которая выведет все натуральные числа от 1 до N. 
+// N = 5 -> "1,2,3,4,5"
+// N = 6 -> "1,2,3,4,5,6"
+
+double GetNumberFromConsole()
+{
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void WriteNumbers(double lastNumber)
+{
+    int counter = 1;
+    while (counter <= lastNumber)
+    {
+        if (counter + 1 <= lastNumber)
+        Console.Write(counter + ", ");
+        else 
+         Console.Write(counter);
+        counter++;
+    }
+    Console.WriteLine();
+}
+
+WriteNumbers(GetNumberFromConsole());
